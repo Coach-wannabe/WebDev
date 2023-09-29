672b038b66a41b49911d0cccb10e9bab664d0d2a
Coach-wannabe <adiletbolatbekov1@gmail.com>
2023-09-29 14:22:46 +0600

    Adding Lab 3

---
+++ Lab3/test2/app.js
@@ -0,0 +1,67 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+
+// Functions
+function addTodo(event){
+    // Prevent form from submitting
+    event.preventDefault();
+
+    //Todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    
+    //Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class = "fas fa-check></i>';
+    completedButton.innerText = "check";
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    
+    //Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class = "fas fa-trash></i>';
+    trashButton.innerText = 'trash';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    
+    //Append todo list
+    todoList.appendChild(todoDiv);
+
+    //clear todo input value
+    todoInput.value = "";
+}
+
+function deleteCheck(event){
+    const item = event.target;
+
+    // Delete todo
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        // animation
+        todo.classList.add("fall");
+
+        todo.addEventListener("transitionend", function(){
+            todo.remove();
+        })
+        //todo.remove();
+    }
+
+    // check mark
+    if(item.classList[0]  === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
